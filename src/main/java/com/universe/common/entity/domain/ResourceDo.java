319768eee9ba84b1bdef576c0a7eeb5ada9426c9
liuyalou <liuyalou@iboxpay.com>
2019-12-21 18:13:47 +0800

    ResourceDo重新生成

--- src/main/java/com/universe/common/entity/domain/ResourceDo.java
+++ src/main/java/com/universe/common/entity/domain/ResourceDo.java
@@ -1,115 +1,124 @@
 package com.universe.common.entity.domain;
 
-import java.util.Date;
-
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.util.Date;
+
 public class ResourceDo {
 
-  private Integer resourceId;
-  private Integer parentId;
-  private String resourceName;
-  private String resourceUrl;
-  private String resourceIcon;
-  private String type;
-  private Integer priority;
-  private String status;
-  private String permission;
-  private Date createTime;
-  private Date updateTime;
+	private Long resourceId;
 
-  public Integer getResourceId() {
-    return resourceId;
-  }
+	private Long parentId;
 
-  public Integer getParentId() {
-    return parentId;
-  }
+	private String resourceName;
 
-  public String getResourceName() {
-    return resourceName;
-  }
+	private String resourceUrl;
 
-  public String getResourceUrl() {
-    return resourceUrl;
-  }
+	private String resourceIcon;
 
-  public String getResourceIcon() {
-    return resourceIcon;
-  }
+	private String type;
 
-  public String getType() {
-    return type;
-  }
+	private Byte priority;
 
-  public Integer getPriority() {
-    return priority;
-  }
+	private String status;
 
-  public String getStatus() {
-    return status;
-  }
+	private String permission;
 
-  public String getPermission() {
-    return permission;
-  }
+	private Date createTime;
 
-  public Date getCreateTime() {
-    return createTime;
-  }
+	private Date updateTime;
 
-  public Date getUpdateTime() {
-    return updateTime;
-  }
+	public Long getResourceId() {
+		return resourceId;
+	}
 
-  public void setResourceId(Integer resourceId) {
-    this.resourceId = resourceId;
-  }
+	public void setResourceId(Long resourceId) {
+		this.resourceId = resourceId;
+	}
 
-  public void setParentId(Integer parentId) {
-    this.parentId = parentId;
-  }
+	public Long getParentId() {
+		return parentId;
+	}
 
-  public void setResourceName(String resourceName) {
-    this.resourceName = resourceName;
-  }
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
 
-  public void setResourceUrl(String resourceUrl) {
-    this.resourceUrl = resourceUrl;
-  }
+	public String getResourceName() {
+		return resourceName;
+	}
 
-  public void setResourceIcon(String resourceIcon) {
-    this.resourceIcon = resourceIcon;
-  }
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
 
-  public void setType(String type) {
-    this.type = type;
-  }
+	public String getResourceUrl() {
+		return resourceUrl;
+	}
 
-  public void setPriority(Integer priority) {
-    this.priority = priority;
-  }
+	public void setResourceUrl(String resourceUrl) {
+		this.resourceUrl = resourceUrl;
+	}
 
-  public void setStatus(String status) {
-    this.status = status;
-  }
+	public String getResourceIcon() {
+		return resourceIcon;
+	}
 
-  public void setPermission(String permission) {
-    this.permission = permission;
-  }
+	public void setResourceIcon(String resourceIcon) {
+		this.resourceIcon = resourceIcon;
+	}
 
-  public void setCreateTime(Date createTime) {
-    this.createTime = createTime;
-  }
+	public String getType() {
+		return type;
+	}
 
-  public void setUpdateTime(Date updateTime) {
-    this.updateTime = updateTime;
-  }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-  @Override
-  public String toString() {
-    return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-  }
+	public Byte getPriority() {
+		return priority;
+	}
 
+	public void setPriority(Byte priority) {
+		this.priority = priority;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPermission() {
+		return permission;
+	}
+
+	public void setPermission(String permission) {
+		this.permission = permission;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
